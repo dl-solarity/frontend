524fd4837b562ee3cbe7d5b9b5640ab8b622a954
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2023-08-03 03:43:24 +0300

    added decode in auto mode

--- forms/AbiDecodeForm.vue
+++ forms/AbiDecodeForm.vue
@@ -13,41 +13,96 @@
         v-model="form.hasFuncSelector"
         :label="$t('abi-decode-form.has-func-selector-label')"
       />
-      <radio-button-field v-model="form.decodeMode" :options="decodeOptions" />
+      <radio-button-field
+        v-model="form.decodeMode"
+        :options="decodeModeOptions"
+      />
+    </div>
+    <div v-if="args.length || errorMessage" class="abi-decode-form__output">
+      <template v-if="args.length">
+        <h2>{{ $t('abi-decode-form.output-title') }}</h2>
+        <div class="abi-decode-form__args_wrp">
+          <div v-for="arg in args" :key="arg.id" class="abi-decode-form__arg">
+            <select-field
+              v-model="arg.type"
+              :readonly="form.decodeMode === DECODE_MODES.auto"
+              :label="$t('abi-decode-form.arg-type-label')"
+              :placeholder="$t('abi-decode-form.arg-type-placeholder')"
+              :value-options="
+                Object.values(ETHEREUM_TYPES).map(v => ({ value: v, title: v }))
+              "
+            />
+            <input-field
+              v-model="arg.value"
+              readonly
+              :label="$t('abi-decode-form.arg-value-label')"
+              :placeholder="$t('abi-decode-form.arg-value-placeholder')"
+            />
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        <p class="abi-decode-form__error-msg">
+          {{ $t('abi-decode-form.error-msg', { msg: errorMessage }) }}
+        </p>
+      </template>
     </div>
   </form>
 </template>
 
 <script lang="ts" setup>
 import { useFormValidation } from '@/composables'
-import { CheckboxField, RadioButtonField, TextareaField } from '@/fields'
-import { required, hex } from '@/helpers'
+import { ETHEREUM_TYPES } from '@/enums'
+import {
+  CheckboxField,
+  InputField,
+  RadioButtonField,
+  SelectField,
+  TextareaField,
+} from '@/fields'
+import { ErrorHandler, required, hex } from '@/helpers'
 import { type FieldOption } from '@/types'
-import { computed, reactive } from 'vue'
+import { guessAbiEncodedData } from '@openchainxyz/abi-guesser'
+import { AbiCoder } from 'ethers'
+import { v4 as uuidv4 } from 'uuid'
+import { computed, reactive, ref, watch } from 'vue'
 import { i18n } from '~/plugins/localization'
+
+type FuncArg = {
+  id: string
+  type: string
+  value: string
+}
 
 enum DECODE_MODES {
   auto = 'auto',
   manual = 'manual',
 }
 
+defineProps<{
+  title: string
+}>()
+
 const { t } = i18n.global
 
-const decodeOptions = computed<FieldOption[]>(() => [
+const args = ref<FuncArg[]>([])
+const errorMessage = ref('')
+
+const decodeModeOptions = computed<FieldOption[]>(() => [
   {
     value: DECODE_MODES.auto,
-    title: t('abi-decode-form.params-option-title--auto'),
+    title: t('abi-decode-form.decode-mode-option-title--auto'),
   },
   {
     value: DECODE_MODES.manual,
-    title: t('abi-decode-form.params-option-title--manually'),
+    title: t('abi-decode-form.decode-mode-option-title--manual'),
   },
 ])
 
 const form = reactive({
   abiEncoding: '',
   hasFuncSelector: false,
-  decodeMode: decodeOptions.value[0].value,
+  decodeMode: decodeModeOptions.value[0].value,
 })
 const rules = computed(() => ({
   abiEncoding: { required, hex },
@@ -59,9 +114,44 @@ const { getFieldErrorMessage, isFieldsValid, touchField } = useFormValidation(
   rules,
 )
 
-defineProps<{
-  title: string
-}>()
+const decode = () => {
+  let types: string[] = []
+
+  if (form.decodeMode === DECODE_MODES.auto) {
+    const paramTypes = guessAbiEncodedData(form.abiEncoding)
+    if (!paramTypes) throw new Error('failed guess params types')
+
+    types = paramTypes.map(type => type.format())
+  }
+
+  const values = AbiCoder.defaultAbiCoder().decode(types, form.abiEncoding)
+
+  args.value = types.map((type, idx) => ({
+    id: uuidv4(),
+    type: type,
+    value: String(values[idx]),
+  }))
+}
+
+const onFormChange = () => {
+  if (!isFieldsValid.value) {
+    args.value.length = 0
+    errorMessage.value = ''
+    return
+  }
+
+  try {
+    decode()
+    errorMessage.value = ''
+  } catch (error) {
+    args.value.length = 0
+    errorMessage.value =
+      error instanceof Error ? error.message : t('abi-decode-form.error-msg--unknown')
+    ErrorHandler.process(error)
+  }
+}
+
+watch(form, onFormChange)
 </script>
 
 <style lang="scss" scoped>
@@ -79,5 +169,28 @@ defineProps<{
 .abi-decode-form__input {
   padding-bottom: toRem(40);
   border-bottom: toRem(1) solid var(--text-secondary-main);
+}
+
+.abi-decode-form__args_wrp {
+  display: grid;
+  grid-gap: toRem(16);
+
+  @include respond-to(small) {
+    grid-gap: toRem(48);
+  }
+}
+
+.abi-decode-form__arg {
+  display: grid;
+  grid-gap: toRem(16);
+  grid-template-columns: minmax(toRem(140), 20%) 1fr;
+
+  @include respond-to(small) {
+    grid-template-columns: auto;
+  }
+}
+
+.abi-decode-form__error-msg {
+  color: var(--error-main);
 }
 </style>
