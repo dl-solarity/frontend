8876c2f9858bc19cdb1afb5c06e69ebf2618679e
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2023-09-04 18:02:50 +0300

    fix hash-function-form

--- helpers/hash-functions.helpers.ts
+++ helpers/hash-functions.helpers.ts
@@ -1,29 +1,39 @@
-import { ethers } from 'ethers'
+import {
+  hexlify,
+  keccak256 as _keccak256,
+  sha256 as _sha256,
+  ripemd160 as _ripemd160,
+  toBeHex,
+  toUtf8Bytes,
+} from 'ethers'
 import { type HashFunction } from '@/types'
 
 export function keccak256(
   ...[str, type]: Parameters<HashFunction>
 ): ReturnType<HashFunction> {
-  const dataHexString = ethers.keccak256(
-    type === 'text' ? ethers.toUtf8Bytes(str) : ethers.toBeHex(str),
+  const dataHexString = _keccak256(
+    type === 'text' ? toUtf8Bytes(str) : toBeHex(str),
   )
-  return ethers.hexlify(dataHexString)
+
+  return hexlify(dataHexString)
 }
 
 export function sha256(
   ...[str, type]: Parameters<HashFunction>
 ): ReturnType<HashFunction> {
-  const dataHexString = ethers.sha256(
-    type === 'text' ? ethers.toUtf8Bytes(str) : ethers.toBeHex(str),
+  const dataHexString = _sha256(
+    type === 'text' ? toUtf8Bytes(str) : toBeHex(str),
   )
-  return ethers.hexlify(dataHexString)
+
+  return hexlify(dataHexString)
 }
 
 export function ripemd160(
   ...[str, type]: Parameters<HashFunction>
 ): ReturnType<HashFunction> {
-  const dataHexString = ethers.ripemd160(
-    type === 'text' ? ethers.toUtf8Bytes(str) : ethers.toBeHex(str),
+  const dataHexString = _ripemd160(
+    type === 'text' ? toUtf8Bytes(str) : toBeHex(str),
   )
-  return ethers.hexlify(dataHexString)
+
+  return hexlify(dataHexString)
 }
