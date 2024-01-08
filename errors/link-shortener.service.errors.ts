19f09d21c32c3add7ad73e71cf6184beb53e4f83
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2024-01-08 13:23:29 +0200

    added todos

--- errors/link-shortener.service.errors.ts
+++ errors/link-shortener.service.errors.ts
@@ -1,9 +1,11 @@
 import { RuntimeError } from './runtime-errors'
 
 export class CreateLinkFetchError extends RuntimeError {
+  // TODO: refactoring by web-kit
   message = 'failed to create link'
 }
 
 export class GetDataByLinkFetchError extends RuntimeError {
+  // TODO: refactoring by web-kit
   message = 'failed to get data by link'
 }
--- errors/runtime-errors.ts
+++ errors/runtime-errors.ts
@@ -1,21 +1,26 @@
 export class RuntimeError extends Error {}
 
 export class FunctionSignatureFetchError extends RuntimeError {
+  // TODO: refactoring by web-kit
   message = 'failed to get function signature'
 }
 
 export class FunctionFragmentGuessError extends RuntimeError {
+  // TODO: refactoring by web-kit
   message = 'failed to guess function fragment'
 }
 
 export class ParamTypesGuessError extends RuntimeError {
+  // TODO: refactoring by web-kit
   message = 'failed guess params types'
 }
 
 export class AbiDecodeError extends RuntimeError {
+  // TODO: refactoring by web-kit
   message = 'failed to decode abi'
 }
 
 export class IncompatibleDataReceivedError extends RuntimeError {
+  // TODO: refactoring by web-kit
   message = 'incompatible data received'
 }
